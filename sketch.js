c7b89149d3f06451da7cffa1bb73a90c005224db
Paolinha2B <00001088653595sp@al.educacao.sp.gov.br>
2024-06-14 14:27:12 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup () {
+    createCanvas(600, 600);
+    background("black")
+  }
+  
+  function draw(){
+   stroke("purple");
+   fill("green");
+  
+    if (mouseIsPressed){
+      rect(mouseX, mouseY, 20, 35);
+    }
+    
+  }
